054ff696ebf9a1a1d5c92bbbc4a3b3aeb7435a4d
howard-cui-itrazo-tracetech <howard@itrazotracetech.com>
2024-08-31 12:48:21 +1000

    feat: build the basic layout of the pages

---
+++ DiaryApp/Migrations/20240831010849_AddedSeedingDataDiaryEntry.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace DiaryApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedSeedingDataDiaryEntry : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "DiaryEntries",
+                columns: new[] { "Id", "Content", "Created", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Went hiking with Joe!", new DateTime(2024, 8, 31, 11, 8, 49, 463, DateTimeKind.Local).AddTicks(3012), "Went Hiking" },
+                    { 2, "Went shopping with Joe!", new DateTime(2024, 8, 31, 11, 8, 49, 463, DateTimeKind.Local).AddTicks(3067), "Went Shopping" },
+                    { 3, "Went diving with Joe!", new DateTime(2024, 8, 31, 11, 8, 49, 463, DateTimeKind.Local).AddTicks(3069), "Went Diving" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "DiaryEntries",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "DiaryEntries",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "DiaryEntries",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
